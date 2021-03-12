cca95096d9a46abf2cb566b12c907da199aad27b
alinaT95 <alina.t@tonlabs.io>
2021-03-12 13:09:15 +0300

    add more public stuff

--- TonNfcClientSwift/Classes/api/utils/ByteArrayAndHexHelper.swift
+++ TonNfcClientSwift/Classes/api/utils/ByteArrayAndHexHelper.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 
-class ByteArrayAndHexHelper {
+public class ByteArrayAndHexHelper {
     
-    static func digitalStrIntoAsciiUInt8Array(digitalStr : String) -> [UInt8]{
+    public static func digitalStrIntoAsciiUInt8Array(digitalStr : String) -> [UInt8]{
       var bytes = [UInt8]()
       for s in digitalStr {
         if let byte = UInt8(String(s)) {
@@ -21,7 +21,7 @@ class ByteArrayAndHexHelper {
       return bytes
     }
     
-    static func hexStrToUInt8Array(hexStr: String) -> [UInt8] {
+    public static func hexStrToUInt8Array(hexStr: String) -> [UInt8] {
       var startIndex = hexStr.startIndex
       return (0..<hexStr.count/2).compactMap { _ in
         let endIndex = hexStr.index(after: startIndex)
@@ -30,7 +30,7 @@ class ByteArrayAndHexHelper {
       }
     }
     
-    static func hex(from string: String) -> Data {
+    public static func hex(from string: String) -> Data {
       .init(stride(from: 0, to: string.count, by: 2).map {
         string[string.index(string.startIndex, offsetBy: $0) ... string.index(string.startIndex, offsetBy: $0 + 1)]
       }.map {
@@ -38,7 +38,7 @@ class ByteArrayAndHexHelper {
       })
     }
     
-    static func makeShort(src: [UInt8], srcOff : Int) -> Int {
+    public static func makeShort(src: [UInt8], srcOff : Int) -> Int {
       // if (srcOff < 0  ||  src.length < (srcOff + 2))
       //   throw new IllegalArgumentException("Bad args!");
       let b0 = Int(src[srcOff] & 0xFF);
--- TonNfcClientSwift/Classes/smartCard/TonWalletAppletConstants.swift
+++ TonNfcClientSwift/Classes/smartCard/TonWalletAppletConstants.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-class TonWalletAppletConstants {
-    static let APP_INSTALLED: UInt8 = 0x07
-    static let APP_PERSONALIZED: UInt8 =  0x17
-    static let APP_WAITE_AUTHORIZATION_MODE: UInt8 = 0x27
-    static let APP_DELETE_KEY_FROM_KEYCHAIN_MODE: UInt8 = 0x37
-    static let APP_BLOCKED_MODE: UInt8 =  0x47
+public class TonWalletAppletConstants {
+    public static let APP_INSTALLED: UInt8 = 0x07
+    public static let APP_PERSONALIZED: UInt8 =  0x17
+    public static let APP_WAITE_AUTHORIZATION_MODE: UInt8 = 0x27
+    public static let APP_DELETE_KEY_FROM_KEYCHAIN_MODE: UInt8 = 0x37
+    public static let APP_BLOCKED_MODE: UInt8 =  0x47
     
-    static let APPLET_STATES = [ APP_PERSONALIZED : "TonWalletApplet is personalized.",
+    public static let APPLET_STATES = [ APP_PERSONALIZED : "TonWalletApplet is personalized.",
                                  APP_BLOCKED_MODE : "TonWalletApplet is blocked.",
                                  APP_WAITE_AUTHORIZATION_MODE : "TonWalletApplet waits two-factor authorization.",
                                  APP_DELETE_KEY_FROM_KEYCHAIN_MODE : "TonWalletApplet is personalized and waits finishing key deleting from keychain.",
@@ -27,36 +27,36 @@ class TonWalletAppletConstants {
     static let WAITE_AUTHORIZATION_STATE = [APP_WAITE_AUTHORIZATION_MODE]
     static let PERSONALIZED_AND_DELETE_STATE = [APP_PERSONALIZED, APP_DELETE_KEY_FROM_KEYCHAIN_MODE]
     
-    static let DEFAULT_PIN:[UInt8] = [0x35, 0x35, 0x35, 0x35]
+    public  static let DEFAULT_PIN:[UInt8] = [0x35, 0x35, 0x35, 0x35]
     
-    static let PK_LEN = 0x20
-    static let SIG_LEN = 0x40
-    static let HMAC_SHA_SIG_SIZE = 32
-    static let SHA_HASH_SIZE = 32
-    static let SAULT_LENGTH = 32
-    static let TRANSACTION_HASH_SIZE = 32
-    static let PIN_SIZE = 4
+    public static let PK_LEN = 0x20
+    public static let SIG_LEN = 0x40
+    public static let HMAC_SHA_SIG_SIZE = 32
+    public static let SHA_HASH_SIZE = 32
+    public static let SAULT_LENGTH = 32
+    public static let TRANSACTION_HASH_SIZE = 32
+    public static let PIN_SIZE = 4
     
-    static let MAX_PIN_TRIES_NUM: UInt8 = 10
-    static let DATA_FOR_SIGNING_MAX_SIZE: UInt16 = 189
-    static let APDU_DATA_MAX_SIZE: UInt16 = 255
-    static let DATA_FOR_SIGNING_MAX_SIZE_FOR_CASE_WITH_PATH: UInt16 = 178
-    static let DATA_PORTION_MAX_SIZE: Int = 128
+    public static let MAX_PIN_TRIES_NUM: UInt8 = 10
+    public static let DATA_FOR_SIGNING_MAX_SIZE: UInt16 = 189
+    public static let APDU_DATA_MAX_SIZE: UInt16 = 255
+    public static let DATA_FOR_SIGNING_MAX_SIZE_FOR_CASE_WITH_PATH: UInt16 = 178
+    public static let DATA_PORTION_MAX_SIZE: Int = 128
     
-    static let PASSWORD_SIZE: UInt16 = 128
-    static let IV_SIZE: UInt16 = 16
-    static let MAX_NUMBER_OF_KEYS_IN_KEYCHAIN: UInt16 = 1023
-    static let MAX_KEY_SIZE_IN_KEYCHAIN: UInt16 = 8192
-    static let KEY_CHAIN_SIZE: UInt16 = 32767
-    static let MAX_IND_SIZE: UInt16 = 10
-    static let COMMON_SECRET_SIZE: UInt16 = 32
-    static let MAX_HMAC_FAIL_TRIES: UInt16 = 20
-    static let KEYCHAIN_KEY_INDEX_LEN = 2
+    public static let PASSWORD_SIZE: UInt16 = 128
+    public static let IV_SIZE: UInt16 = 16
+    public static let MAX_NUMBER_OF_KEYS_IN_KEYCHAIN: UInt16 = 1023
+    public static let MAX_KEY_SIZE_IN_KEYCHAIN: UInt16 = 8192
+    public static let KEY_CHAIN_SIZE: UInt16 = 32767
+    public static let MAX_IND_SIZE: UInt16 = 10
+    public static let COMMON_SECRET_SIZE: UInt16 = 32
+    public static let MAX_HMAC_FAIL_TRIES: UInt16 = 20
+    public static let KEYCHAIN_KEY_INDEX_LEN = 2
     
-    static let DATA_RECOVERY_PORTION_MAX_SIZE: Int = 250
-    static let RECOVERY_DATA_MAX_SIZE: Int = 2048
+    public static let DATA_RECOVERY_PORTION_MAX_SIZE: Int = 250
+    public static let RECOVERY_DATA_MAX_SIZE: Int = 2048
     
-    static let SERIAL_NUMBER_SIZE: Int = 24
+    public static let SERIAL_NUMBER_SIZE: Int = 24
     
     static let GET_RECOVERY_DATA_LEN_LE: Int = 0x02
     static let GET_APP_INFO_LE: Int = 0x01
@@ -73,8 +73,8 @@ class TonWalletAppletConstants {
     static let GET_DELETE_KEY_RECORD_NUM_OF_PACKETS_LE: Int = 0x02
     static let GET_SERIAL_NUMBER_LE: Int = 0x18
     
-    static let DEFAULT_SERIAL_NUMBER: String = "504394802433901126813236"
+    public static let DEFAULT_SERIAL_NUMBER: String = "504394802433901126813236"
     
-    static let EMPTY_SERIAL_NUMBER: String = "empty"
+    public static let EMPTY_SERIAL_NUMBER: String = "empty"
     
 }
